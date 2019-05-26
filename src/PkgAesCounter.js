2ea76817932fd668ebae14fade85c6ae683377da
JK3Y <17938577+JK3Y@users.noreply.github.com>
2019-05-26 11:06:02 -0500

    Removed aes-js as a dependency, using Node's built-in crypto module. Added dependency bigint-buffer for BigInt support to use with IV during AES decryption.

--- src/PkgAesCounter.js
+++ src/PkgAesCounter.js
@@ -1,35 +1,36 @@
 "use strict";
 exports.__esModule = true;
-var aesjs = require("aes-js");
+var crypto = require("crypto");
+var bigintbuf = require("bigint-buffer");
 var PkgAesCounter = /** @class */ (function () {
     function PkgAesCounter(key, iv) {
         this.keySize = 128;
         this.blockOffset = -1;
         this.aes = null;
         this.key = key;
-        this.iv = iv;
+        this.iv = Buffer.from(iv).toString('hex');
     }
     PkgAesCounter.prototype.setOffset = function (offset) {
         if (offset === this.blockOffset) {
             return;
         }
-        var startCounter = this.iv;
+        var startCounter = Buffer.from(this.iv, 'hex');
         this.blockOffset = 0;
         var count = offset / 16;
         if (count > 0) {
-            startCounter += count;
+            var intval = bigintbuf.toBigIntBE(startCounter);
+            startCounter = bigintbuf.toBufferBE(intval + BigInt(count), 16);
             this.blockOffset += count * 16;
         }
         if (this.aes) {
             this.aes = null;
         }
-        var counter = new aesjs.Counter(startCounter);
-        this.aes = new aesjs.ModeOfOperation.ctr(this.key, counter);
+        this.aes = crypto.createCipheriv('aes-128-ctr', this.key, startCounter);
     };
     PkgAesCounter.prototype.decrypt = function (offset, data) {
         this.setOffset(offset);
         this.blockOffset += data.length;
-        var decryptedData = this.aes.decrypt(data);
+        var decryptedData = this.aes.update(data);
         return decryptedData;
     };
     return PkgAesCounter;
--- src/PkgAesCounter.ts
+++ src/PkgAesCounter.ts
@@ -1,37 +1,38 @@
-import * as aesjs from 'aes-js'
+import * as crypto from 'crypto'
+import * as bigintbuf from 'bigint-buffer'
+
 export class PkgAesCounter {
   public key: Uint8Array
   public keySize: number = 128
-  public iv: number
+  public iv: string
   public blockOffset: number = -1
   private aes: any = null
 
-  constructor(key: Uint8Array, iv: number) {
+  constructor(key: Uint8Array, iv: Uint8Array) {
     this.key = key
-    this.iv = iv
+    this.iv = Buffer.from(iv).toString('hex')
   }
 
   public setOffset(offset: number) {
     if (offset === this.blockOffset) { return }
 
-    let startCounter = this.iv
+    let startCounter = Buffer.from(this.iv, 'hex')
     this.blockOffset = 0
     let count = offset / 16
 
     if (count > 0) {
-      startCounter += count
+      let intval = bigintbuf.toBigIntBE(startCounter)
+      startCounter = bigintbuf.toBufferBE(intval + BigInt(count), 16)
       this.blockOffset += count * 16
     }
     if (this.aes) { this.aes = null }
-    let counter = new aesjs.Counter(startCounter)
-    this.aes = new aesjs.ModeOfOperation.ctr(this.key, counter)
+    this.aes = crypto.createCipheriv('aes-128-ctr', this.key, startCounter)
   }
 
-  public decrypt(offset: number, data: Uint8Array) {
+  public decrypt(offset: number, data: Buffer) {
     this.setOffset(offset)
-
     this.blockOffset += data.length
-    let decryptedData = this.aes.decrypt(data)
+    let decryptedData = this.aes.update(data)
     return decryptedData
   }
 }
